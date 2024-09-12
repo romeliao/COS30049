6b5cff199eca7f256a7be3672918119e2ea0029c
romeliao <jerome.liao2003@gmail.com>
2024-09-12 14:02:42 +1000

    added in line comments and adjusted diagrams

--- weather_prediction.py
+++ weather_prediction.py
@@ -12,7 +12,6 @@ import os
 import time 
 
 from sklearn.preprocessing import MinMaxScaler
-from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, LSTM, Bidirectional, InputLayer
@@ -27,43 +26,63 @@ from tensorflow.keras.layers import Dense, Dropout, LSTM, Bidirectional, InputLa
 #------------------------------------------------------------------------------
 # parameters
 #------------------------------------------------------------------------------
-COMPANY = "Weather Prediction"
-#TRAIN_START = '2008-12-01'
-#TRAIN_END = '2010-12-01'
-FILL_METHOD = 'ffill'
-SPLIT_METHOD = 'ratio'
-DROP_THRESHOLD = 0.7
-FEATURES = ['Location', 'MinTemp','MaxTemp','Rainfall','Evaporation','Sunshine','RainToday','RainTomorrow']
+FILL_METHOD = 'ffill'           #this is to specify how to handle missing values in the dataset
+SPLIT_METHOD = 'ratio'          #this indicates that the dataset will be split into training and testing based on the specified ratio
+DROP_THRESHOLD = 0.7            #if the column is more than 70% missing value it will be dropped from the dataset 
+Output_DIagram = 'Diagrams'     #file to save diagrams to
+Bar_GraphFeature = 'RainToday'  # to choose what feature to show on the bar graph
+
+#Scatter plot variables 
+X_COL='MinTemp'
+Y_COL='MaxTemp'
+
+#features that user wants to call and analyse 
+#this is to represent the specific columns from the dataset that will be extracted for analysis
+FEATURES = ['Date','Location', 'MinTemp','MaxTemp','Rainfall','Evaporation','Sunshine','RainToday','RainTomorrow']
+
+#columns to normalize
+#this refers to the columns in the dataset that will be normalized and adjuust the values to a standard cale
+# to improve the performace of the machine learning algorithms
 NORMALIZE_COLUMN = ['MinTemp','MaxTemp','Rainfall','Evaporation','Sunshine','WindGustDir',
             'WindGustSpeed','WindDir9am','WindDir3pm','WindSpeed9am','WindSpeed3pm','Humidity9am','Humidity3pm','Pressure9am','Pressure3pm','Cloud9am','Cloud3pm',
             'Temp9am','Temp3pm','RainToday','RainTomorrow']
 
+#file path is to indicate where the dataset in being read from
+#the dataset will be stored in the variable df for further processing 
 FILE_PATH = df = pd.read_csv('data/weatherAUS.csv') 
-
-# save processes data file path
-date_now = time.strftime("%Y-%m-%d")
-data_filename = os.path.join("data",f"{COMPANY}_{date_now}.csv") #processed data
-file_name = f"{date_now}_{COMPANY}"
 
 #------------------------------------------------------------------------------
 # format lables in the dataset 
 #------------------------------------------------------------------------------
+#this function takse two arguments which is DF and Categorical columns 
+# df is the datafram containing the dateset 
+# categorical columns is a list of column names that contain categorical data non numeric
 def encode_categorical(df, categorical_columns):
+
     # Initialize LabelEncoder for categorical columns
+    #this is to create to store the lable encoder objets for each categorical column
     label_encoders = {}
+
+    #for each column in the list the lable encoder will be created 
     for col in categorical_columns:
         le = LabelEncoder()
         df[col] = le.fit_transform(df[col].astype(str))  # Ensure all values are strings for consistent encoding
         label_encoders[col] = le  # Store the label encoders for possible inverse_transform later
+    
     return df, label_encoders
 
 #------------------------------------------------------------------------------
 # load and process data function
 #------------------------------------------------------------------------------
 def process_weather_data(file_path,fill_method = 'ffill', drop_threshold = 0, normalize_columns=None, features=None):
+    
+    #the dataset is loaded into the dataframe 
     df = pd.read_csv('data/weatherAUS.csv') 
 
     #handling missing values
+    # mean is to replace missing values with the column mean 
+    # ffill is foward fill the missing values 
+    # drop is to drop the missing rows with missing values 
     if fill_method =='mean':
         df.fillna(df.mean(), inplace=True)
     elif fill_method == 'ffill':
@@ -74,11 +93,19 @@ def process_weather_data(file_path,fill_method = 'ffill', drop_threshold = 0, no
         raise ValueError("Invalid fill method. Choose 'mean', 'ffill', or 'drop'.")
 
     # Drop columns with more missing data than the threshold
+    #the columns with more missing data than the specified treshold are dropped
+    #if the threshold is 0.7 any column with more than 70% missing values will be remoced
     df.dropna(thresh=int(drop_threshold * len(df)), axis=1, inplace=True)
 
+     # Ensure the Date column is in datetime format
+    if 'Date' in df.columns:
+        df['Date'] = pd.to_datetime(df['Date'], errors='coerce')  # Coerce errors to handle invalid dates
+
+    #this is the columns to be encoded as categorical features 
+    # the encode_categorical function is to transform these categorical columns into numerical lables usign the encoder 
     if features:
         features_to_encode = [col for col in features if col not in 
-                              ['Location', 'WindGustDir', 'WindDir9am', 'WindDir3pm','RainToday','RainTomorrow']]
+                              ['Date','Location', 'WindGustDir', 'WindDir9am', 'WindDir3pm','RainToday','RainTomorrow']]
         df, label_encoders = encode_categorical(df, features_to_encode)
 
     if normalize_columns:
@@ -99,11 +126,19 @@ def process_weather_data(file_path,fill_method = 'ffill', drop_threshold = 0, no
             print("No numerical columns to normalize.")
 
     # Filter the DataFrame to include only the columns specified in DISPLAY_COLUMNS
-    filtered_df = df[FEATURES]
+    # This function is used to filter the data fram to include only the specified 
+    # Columns in features if is not provided it will return the entire dataframe 
+    filtered_df = df[features] if features else df
 
     return filtered_df
 #------------------------------------------------------------------------------
 # Call Processing data function 
+# this is to load, clean and process the weather data bsed on the parameters provided 
+# the file path is which the data set file is found 
+# the fill method is for filling the missing vales 
+# the drop threshold is to drop the columns with excessive missing data 
+# the normalize argument is to ensure the columns are normalize 
+# the features will indicate which columns are being called
 #------------------------------------------------------------------------------
 processed_df = process_weather_data(file_path=FILE_PATH,fill_method=FILL_METHOD,
     drop_threshold=DROP_THRESHOLD,normalize_columns=NORMALIZE_COLUMN,features=FEATURES
@@ -114,32 +149,51 @@ processed_df = process_weather_data(file_path=FILE_PATH,fill_method=FILL_METHOD,
 # print("\n",df.columns)                      #show what columns are there 
 # df.info()                                   #show columns info
 #------------------------------------------------------------------------------
+#this is to configure the pandas to display all the columns of the dataframe when 
+#printing rather than truncating the view 
+#the print(processed_df.head(10)) prints out the number of rows that is being called 
 print("\n")
 pd.set_option('display.max_columns', None)
 print(processed_df.head(10))
 
+#to check the top 5 most frequect values for the columns
+#this is to iterate through all the columns in the original dataframe 
+for col in df.columns:
+    print('\n')
+    print(col)
+    print('-'*20)
+    print(df[col].value_counts(normalize=True).head())
+
 #------------------------------------------------------------------------------
 # Histogram function
 #------------------------------------------------------------------------------
-def plot_histogram(df, output_dir ='Diagrams', figsize=(12,18)):
+def plot_histogram(df, output_dir =Output_DIagram, figsize=(12,18)):
 
     #create the directory if it doesn't exist
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
-    #Get the continuous 
+    #Get the continuous
+    #this finds the columns in the dataframe that have a numeric datatype
+    # these are the columns for which histograms will be plotted
     continuous_columns = [col for col in df.columns if df[col].dtype != object]
 
     #set up the subplot grid 
+    #this creates a grid of subplots with 3 rows and columns using matplotlibs subplots 
+    #Flattens the axes array to make it easier to iterate over in a single dimension.
     fig, axes = plt.subplots(nrows = 3, ncols = 3, figsize=figsize)
     axes = axes.reshape(-1)
 
-    #plot a histogram for each of the columns called 
+    #plot a histogram for each of the columns called
+    #this function uses seaborn histplot to creat a histogram for each column
+    #kde parameters adda kernel density estimate to the plot 
     for i , col in enumerate(continuous_columns):
         sns.histplot(df[col], ax=axes[i], kde=True)
         axes[i].set_title(f'Distribution of {col}')
 
-    #adjust layout and save the figure 
+    #adjust layout and save the figure
+    #this adjust the layout of the subplots to make sure they are neatly arranged and not 
+    #overlapping with a padding of 2.0
     fig.tight_layout(pad=2.0)
     plt.suptitle("Histogram of Columns", fontsize = 16, y =1.02)
 
@@ -149,41 +203,76 @@ def plot_histogram(df, output_dir ='Diagrams', figsize=(12,18)):
     plt.show()
 #------------------------------------------------------------------------------
 # Bar graph function function for rainy days
+# this function is to help display the processed data into a bar graph 
+# this function takes parameters like DF and output_dir 
+# which is where the diagram will be saved to 
 #------------------------------------------------------------------------------
-def anayze_rainy_days_by_location(df, output_dir='Diagrams'):
+def Bar_Graph(df, output_dir=Output_DIagram):
+
     #ensure the RainToday column is in binary format (1 for 'Yes', 0 for 'No')
-    df['RainToday'] = df['RainToday'].apply(lambda x: 1 if x== 'Yes' else 0)
+    #this step is to ensure that the column is suitable for numerical aggregation and plotting 
+    df[Bar_GraphFeature] = df[Bar_GraphFeature].apply(lambda x: 1 if x== 'Yes' else 0)
 
     #group by location an sum the rainToday column to get the number of rainy days per location 
+    #this groups the dataframe by the location colume 
+    # and it aggregates the value of the chosen 
+    # feature summing them up for each location 
     df_rain_by_location = df.groupby(by='Location').sum()
-    df_rain_by_location = df_rain_by_location[['RainToday']]
+    df_rain_by_location = df_rain_by_location[[Bar_GraphFeature]]
 
-    #plotting the bar graph 
+    #plotting the bar graph
+    # the x axis is repensented by the feature that has been chosen to be shown on the bar graph
+    # the y axis is to represent the location 
+    #data=df_rain_by_location.sort_values('RainToday', ascending=False): this is used to sort the data by 
+    # the number of days by descending order example number of rainy days 
+    # oreint h is to specifies a horizontal orientation for th bar plot 
+    # palette=' creast' is used to appl a color palette for the bars
     plt.figure(figsize=(8,12))
-    sns.barplot(x='RainToday', y=df_rain_by_location.index,
-                data=df_rain_by_location.sort_values('RainToday', ascending=False),
-                orient='h', palette='crest')
-    plt.xlabel('Number of Rainy Days')
-    plt.title('Rainy Days by Location')
+    sns.barplot(x=Bar_GraphFeature, y=df_rain_by_location.index,
+                data=df_rain_by_location.sort_values(Bar_GraphFeature, 
+                        ascending=False),orient='h', palette='crest')
+    plt.xlabel(f'Number of {Bar_GraphFeature}')
+    plt.title(f'{Bar_GraphFeature} by Location')
     plt.tight_layout()
 
-    #save the figure 
-    output_path = os.path.join(output_dir,'rainy_days_by_loc.png')
+    #save the figure
+    #this is used to create the file with a specific name and save it to the 
+    #designated location 
+    output_path = os.path.join(output_dir,f'{Bar_GraphFeature}by_loc.png')
     plt.savefig(output_path, facecolor='white', dpi=100)
     plt.show()
 
 #------------------------------------------------------------------------------
 # Heatmap function
+# this heatmap plot takes in multiple parameters
+# the df is used for the datafram which will be used to plot the diagram
+# the output dir is used to save the diagram to the right file path 
+#figsize is the sige of the figure is set to 
+#focus_cols is an optional list of column to focus on if provided only these columns will be included in the heatmap 
 #------------------------------------------------------------------------------
-def plot_heatmap(df, output_dir='Diagrams', figsize=(12,10), focus_cols=None):
+def plot_heatmap(df, output_dir=Output_DIagram, figsize=(12,10), focus_cols=None):
+    
+    #this is used to provide the data fram and it is filtered to include only these 
+    #columns. to allow the heatmap for a specific subset of columns to be generated
     if focus_cols:
         df = df[focus_cols]
 
+    #Computes the correlation matrix for the DataFrame. 
+    # The correlation matrix is a table showing correlation coefficients 
+    # between pairs of columns. Values range from -
+    # 1 (perfect negative correlation) to 1 (perfect positive correlation).
     corr_matrix = df.corr()
 
     #plotting the heatmap
+    #Uses Seaborn’s heatmap function to plot the correlation matrix:
+    #corr_matrix: The correlation matrix to be visualized.
+    #annot=True: Annotates each cell with the numeric value of the correlation coefficient.
+    #cmap='coolwarm': Applies the 'coolwarm' color map, which provides a gradient from blue (negative correlations) to red (positive correlations).
+    #fmt='.2f': Formats the annotation text to two decimal places.
+    #vmin=-1, vmax=1: Sets the color scale limits to -1 and 1 to standardize the visualization range.
     plt.figure(figsize=figsize)
-    sns.heatmap(corr_matrix, annot=True, cmap='coolwarm', fmt='.2f', vmin=-1, vmax=1)
+    sns.heatmap(corr_matrix, annot=True, cmap='coolwarm', 
+                fmt='.2f', vmin=-1, vmax=1)
     
     #save heatmap diagram to the right path
     plt.title('Correlation Heatmap')
@@ -193,14 +282,22 @@ def plot_heatmap(df, output_dir='Diagrams', figsize=(12,10), focus_cols=None):
 
 #------------------------------------------------------------------------------
 # scatter plot diagram
+# the scatter plot diagram takes in parameters like 
+# df,x_col,y_col output_dir, figsize, and hue 
 #------------------------------------------------------------------------------
-def scatter_plot(df,x_col,y_col ,output_dir='Diagrams', figsize=(8,6), hue=None):
+def scatter_plot(df,x_col,y_col ,output_dir=Output_DIagram, figsize=(8,6), hue=None):
 
     #create directory if it doesn't exist 
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
-    #plotting scatter plot 
+    #plotting scatter plot
+    # data = df is used to specifi the dataframe containing the data 
+    # x=x_col is used to set the x axis to the values from  x_col
+    # y=y_col is used to set the y axis to the values from y_col
+    #hue is used colors the points based on categories or values in the hue column if
+    #hue is none all points will be the same color 
+    # the palette='viridis': applies the virdis color palette to the plot
     plt.figure(figsize=figsize)
     sns.scatterplot(data=df, x=x_col, y=y_col,hue=hue, palette='viridis')
     plt.title(f'Scatter Plot: {x_col} vs {y_col}', fontsize=14)
@@ -215,13 +312,42 @@ def scatter_plot(df,x_col,y_col ,output_dir='Diagrams', figsize=(8,6), hue=None)
     #display the scatter plot graph
     plt.show()
 
+#------------------------------------------------------------------------------
+# box plot diagram
+# df is used to contain the data frame to be ploted 
+# features is a list of column names for wich the bo plots will be created 
+#output dir is the directory where the diagram will be saved
+# figsize is used to specidies the dimensions of the box plot 
+#------------------------------------------------------------------------------
+# Box plot function
+def plot_boxplot(df, features, output_dir=Output_DIagram, figsize=(12,6)):
+
+    # Create the directory if it doesn't exist
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    # Plot the box plot for each feature
+    #Creates a figure with the specified size.
+    #Uses the boxplot method of the DataFrame to create box plots for the columns specified in features. Each column will have its own box plot.
+    #Sets the title of the box plot to "Box Plot of Features".
+    #Adjusts the layout to ensure everything fits well within the figure.
+    plt.figure(figsize=figsize)
+    df[features].boxplot()
+    plt.title("Box Plot of Features", fontsize=16)
+    plt.tight_layout()
+
+    # Save the plot
+    output_path = os.path.join(output_dir, 'boxplot.png')
+    plt.savefig(output_path, facecolor='white', dpi=100)
+    plt.show()
+
 
 
 #------------------------------------------------------------------------------
 # Calling graph functions
 #------------------------------------------------------------------------------
 plot_histogram(processed_df)                    # calling Histogram graph.
-anayze_rainy_days_by_location(processed_df)     # calling graph for rainy days 
+Bar_Graph(processed_df)                         # calling bar graph
 plot_heatmap(processed_df)                      # calling heat map graph for the functiong being called
 
 #calling scatter plot diagram 
@@ -229,6 +355,8 @@ plot_heatmap(processed_df)                      # calling heat map graph for the
 #example like x_col='Sunshine', y_col='Rainfall'
 
 #scatter plot for min temp and max temp
-scatter_plot(df=processed_df, x_col='MinTemp', y_col='MaxTemp')
-#scatter plot for rainfall vs evaporation          
-scatter_plot(df=processed_df, x_col='Rainfall', y_col='Evaporation')
+scatter_plot(df=processed_df, x_col=X_COL, y_col=Y_COL)
+
+#calling box plot function 
+plot_boxplot(df=processed_df, features=FEATURES)
+
